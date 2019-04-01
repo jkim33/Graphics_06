9b6fe8062a186766625db13499b975040e9e6cec
jkim33 <jkim33@stuy.edu>
2019-04-01 01:00:44 -0400

    done

---
+++ gmath.h
@@ -0,0 +1,13 @@
+#ifndef GMATH_H
+#define GMATH_H
+
+#include "matrix.h"
+#include "math.h"
+#include "ml6.h"
+
+//vector functions
+void normalize( double *vector );
+double dot_product( double *a, double *b );
+double *calculate_normal(struct matrix *polygons, int i);
+
+#endif
